af48e583e72435f23e9fd306148ac973381ec0ca
Gent S <gentsemaj@live.com>
2021-12-31 21:55:50 -0500

    Cleanup FixCompilerGeneratedThis

--- ICSharpCode.Decompiler/IL/Transforms/FixCompilerGeneratedThis.cs
+++ ICSharpCode.Decompiler/IL/Transforms/FixCompilerGeneratedThis.cs
@@ -21,42 +21,50 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 		{
 			for (int i = 0; i < block.Instructions.Count; i++)
 			{
-				if(block.Instructions[i].MatchStLoc(out ILVariable var, out ILInstruction val))
+				if(block.Instructions[i].MatchStLoc(out ILVariable vari, out ILInstruction val))
 				{
-					if (!var.Type.Name.Contains("<>c__CompilerGenerated"))
+					// not sure how to actually check the attribute for vars
+					if (!vari.Type.Name.Contains("<>c__CompilerGenerated"))
 						continue;
 
+					// ensure instruction is newobj
 					if (val.OpCode != OpCode.NewObj)
 						continue;
 
+					// ensure instruction is a call instruction with exactly one arg
 					CallInstruction inst = val as CallInstruction;
 					if (inst == null || inst.Arguments.Count != 1)
 						continue;
 
+					// ensure that one arg is `this`
 					ILInstruction arg = inst.Arguments[0];
 					if (!arg.MatchLdThis())
 						continue;
 
+					// ensure the call is to a constructor
 					IMethod method = inst.Method;
 					if (!method.IsConstructor)
 						continue;
 
-					var.Type = (arg as IInstructionWithVariableOperand).Variable.Type;
-					var loads = var.LoadInstructions.ToArray();
+					// set variable type to the same type as `this`
+					vari.Type = (arg as IInstructionWithVariableOperand).Variable.Type;
+
+					// iterate through uses and simplify them
+					var loads = vari.LoadInstructions.ToArray();
 					foreach (var exp in loads)
 					{
 						ILInstruction wrapper = exp.Parent.Parent;
-						if (wrapper != null && MatchTrivialWrapper(wrapper, var.Type))
-							wrapper.ReplaceWith(new LdLoc(var));
-
+						if (wrapper != null && MatchWrapper(wrapper, vari.Type))
+							wrapper.ReplaceWith(new LdLoc(vari));
 					}
 
-					block.Instructions[i].ReplaceWith(new StLoc(var, arg));
+					// replace cgo with a simple reference that will get cleaned up by CopyPropogation
+					block.Instructions[i].ReplaceWith(new StLoc(vari, arg));
 				}
 			}
 		}
 
-		static bool MatchTrivialWrapper(ILInstruction inst, IType thisType)
+		static bool MatchWrapper(ILInstruction inst, IType thisType)
 		{
 			if(inst.MatchLdObj(out ILInstruction targ, out IType type))
 			{
